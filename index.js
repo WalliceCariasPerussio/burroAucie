06dd29eca93d28cde7b3d0d57a6f171831dac6cd
Wallice <wallice@fdcsistemas.com.br>
2022-10-31 13:14:25 -0300

    burro

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const { REST, Routes ,Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const TOKEN = 'ODYxNjIxMTc4NDk3MjM3MDIy.GH9s3h.yCratB21kyRxTfpb6Hac0wO1mqLZRA44FXs_S8';
+const CLIENT_ID = '861621178497237022';
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+  {
+    name: 'senhafdc',
+    description: 'Replies with a random Senha FDC!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
   }
-})
+})();
 
-client.login(process.env.TOKEN)
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }else if (interaction.commandName === 'senhafdc') {
+    // senha e 004420 dia + 20 mes + 11
+    const DIA = new Date().getDate() + 20;
+    const MES = new Date().getMonth() + 11;
+    await interaction.reply(`A senha do dia é: 004420${DIA}${MES}`);
+  }
+});
+
+client.login(TOKEN);
